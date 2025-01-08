a6cedc814e937fef87fa5e7e88271ff4891b0473
dljimenez2324 <dljimenez2324@gmail.com>
2025-01-07 21:07:43 -0800

    for app\issues\page.tsx  we are adding portions to make the field work

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,32 @@
+'use client'
 import { Button, TextArea, TextField } from '@radix-ui/themes'
 import React from 'react'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { Controller, useForm } from 'react-hook-form';
+
+
+
+interface IssueForm {
+    title: string;
+    description: string;
+}
 
 const NewIssuePage = () => {
+
+   const {register, control} = useForm<IssueForm>();
+   console.log(register("title"))
+
   return (
     <>
     <div className='max-w-xl space-y-3'>
-        <TextField.Root size="2" placeholder="Title" />
-        <TextArea placeholder="Description" />
+        <TextField.Root size="2" placeholder="title" {...register('title')} />
+        <Controller
+            name='description'
+            control={control}
+            render={(field) => <SimpleMDE placeholder="Description"  />}
+        />
+        
         <Button>Submit New Issue</Button>
     </div>
     </>
